c4ad1e239389832d01e458f6a366458c04b06588
xKratos1901 <razors-clerics-0m@icloud.com>
2023-06-19 16:16:07 +0300

    fix issues for build job

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,7 +20,7 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-import candour from "./company/candour.jpg"
+import candour from "./company/candour.png"
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
