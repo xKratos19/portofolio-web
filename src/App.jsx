c2f4a3f424938530e92499910b8e46a8a4937f02
xKratos1901 <razors-clerics-0m@icloud.com>
2023-06-19 20:04:28 +0300

    Upgrade: react-ga

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { useEffect } from 'react';
 import { BrowserRouter } from "react-router-dom";
+import ReactGA from 'react-ga';
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
+
+
 const App = () => {
+
+  ReactGA.initialize('G-D94KCFSKJH');
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [])
+
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
